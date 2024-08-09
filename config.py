ea321071daefc921f12ffc858e9fa2314a2b6b72
glee <regulation.lee@gmail.com>
2024-08-09 20:42:00 +0900

    # initial commit

---
+++ config.py
@@ -0,0 +1,132 @@
+import os
+import argparse
+
+def set_parameters():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-sm', "--sample_minute", type=int, default=360)  # SAMPLE_MINUTE
+    parser.add_argument('-dm', "--drop_minute", type=int, default=10)  # DROP_MINUTE
+    parser.add_argument('-st', "--stop_threshold", type=float, default=4.0)  # STOP_THRESHOLD
+    parser.add_argument('-ss', "--stop_sec", type=int, default=30)  # STOP_SEC
+    parser.add_argument('-im', "--interval_min", type=int, default=60)  # interval_min
+    parser.add_argument('-ud', "--update_decay", type=int, default=5)  # update_decay
+
+    parser.add_argument('-eda', "--set_threshold", action='store_true')
+
+    args = parser.parse_args()
+    args.sample_list = [
+        '24341577',
+        '26674039',
+        '29550344',
+        '41290104',
+        '41893039',
+        '44402381',
+        '55716450',
+        '75218248',
+        '75925582',
+        '88344483',
+        '92530466',
+        '93465680'
+    ]
+    args.accidents = [
+        '41290104',
+        '26674039',
+        '41893039',
+        '75218248'
+    ] # '57510162',
+
+    args.non_accidents = [
+        '24341577',
+        '29550344',
+        '44402381',
+        '55495892',
+        '55507348',
+        '55716450',
+        '75925582',
+        '88344483',
+        '91609436',
+        '92530466',
+        '93465680'
+    ]  # '57510162',
+
+    # except records that has alignment issue
+    args.exception = {
+        'cSenAccX': [55507348, 91609436],
+        'cSenAccY': [55507348, 91609436],
+        'cSenAccZ': [],
+        'cSenGyrX': [],
+        'cSenGyrY': [],
+        'cSenGyrZ': [],
+        'cSenAngX': [55507348, 91609436],
+        'cSenAngY': [],
+        'cSenAngZ': [],
+        'AccNorm': [],
+        'zigzag': []
+    }
+
+    args.COLLECT_PERIOD = ['202111', '202201', '202202', '202203', '202204', '202205', '202402']
+
+    # ignore the data that has the number of dataframe for a day less than drop_period
+    args.drop_period = 10 * 60 * args.drop_minute  # drop_period
+    # sample_period: frame interval for computing raw safety index
+    args.sample_period = 10 * 60 * args.sample_minute
+    # args.sample_period = 'per_day'
+    args.stop_period = 10 * args.stop_sec
+
+    args.tgt_abnormal_ratio = [0.25, 0.15]  # [normal, abnormal]
+
+    args.interval = int(args.interval_min / args.sample_minute)
+    args.index_threshold = [0.1, 0.8] # abnormal ratio: counting abnormal frame in 36,000 frame
+
+    args.SORT_PATH = 'data/' + 'SORTED/'
+    args.SAMPLE_PATH = 'data/' + 'SAMPLED/'
+    args.ACCIDENT_PATH = 'data/' + 'raw_accident/'
+    args.EDA_PATH = 'data/' + 'EDA/'
+    args.TRAIN_PATH = 'data/' + 'DATASET/'
+    if not os.path.exists(args.SORT_PATH):
+        os.mkdir(args.SORT_PATH)
+    if not os.path.exists(args.SAMPLE_PATH):
+        os.mkdir(args.SAMPLE_PATH)
+    if not os.path.exists(args.EDA_PATH):
+        os.mkdir(args.EDA_PATH)
+    if not os.path.exists(args.TRAIN_PATH):
+        os.mkdir(args.TRAIN_PATH)
+
+    return args
+
+
+def set_header(target_period):
+    BASIC_HEADER = ['cSenID', 'cSenDate', 'cSenTime', 'cSenType',
+                    'cSenAccX', 'cSenAccY', 'cSenAccZ',
+                    'cSenGyrX', 'cSenGyrY', 'cSenGyrZ',
+                    'cSenAngX', 'cSenAngY', 'cSenAngZ']
+    GPS_HEADER = ['gpsLatitude', 'gpsLongitude', 'gpsAltitude', 'gpsSpeed']
+    ADD_SEN_HEADER = ['cSenAccX2', 'cSenAccY2', 'cSenAccZ2', 'cSenMageX', 'cSenMageY', 'cSenMageZ']
+    TAIL_HEADER = ['datelog', 'riskChkLevel_1', 'riskChkLevel_2']
+
+    if target_period == '202111':
+        HEADER_LIST = BASIC_HEADER + ['cSenTemp'] + GPS_HEADER + TAIL_HEADER
+    elif target_period == '202202':
+        HEADER_LIST = BASIC_HEADER + ['cSenTemp'] + GPS_HEADER + ['movingDistance'] + TAIL_HEADER
+    elif target_period == '202402':
+        HEADER_LIST = (BASIC_HEADER + ADD_SEN_HEADER + ['cSenTemp']
+                       + GPS_HEADER + ['movingDistance'] + TAIL_HEADER + ['cDriverId'])
+
+    return HEADER_LIST
+
+
+def load_threshold(args, tgt_feat):
+    if tgt_feat == 'cSenAccX':
+        if args.tgt_abnormal_ratio == [0.2, 0.2]:
+            preset_threshold = [0.1661, -0.0366]
+            abnormal_ratio = [20.436, 36.146]
+        elif args.tgt_abnormal_ratio == [0.3, 0.3]:
+            preset_threshold = [0.1154, -0.0366]
+            abnormal_ratio = [30.974, 36.146]
+        elif args.tgt_abnormal_ratio == [0.4, 0.4]:
+            preset_threshold = [0.0901, -0.0366]
+            abnormal_ratio = [39.137, 36.146]
+    else:
+        preset_threshold = 0
+        abnormal_ratio = 0
+
+    return preset_threshold, abnormal_ratio
---
+++ data_preprocessing.py
@@ -0,0 +1,165 @@
+from joblib import Parallel, delayed
+from io import StringIO
+import os
+import glob
+from tqdm import tqdm
+from config import *
+from utils import *
+
+
+def sort_rawdata(args, raw_path, period):
+    def process_file(file_path, args, period):
+        with open(file_path, 'rb') as f:
+            binary_data = f.read()
+        csv_string = binary_data.decode('utf-8').replace('\x00', '')
+
+        na_values = ['\\N', r'\N']
+        df = pd.read_csv(StringIO(csv_string), header=None, na_values=na_values, keep_default_na=True)
+        if period == '202111':
+            df.columns = set_header('202111')
+        else:
+            if df.shape[1] == 21:
+                df.columns = set_header('202201')
+            elif df.shape[1] == 22:
+                df.columns = set_header('202202')
+            else:
+                df.columns = set_header('202402')
+        # df.columns = args.HEADER_LIST
+        df['cSenID'] = df['cSenID'].astype(str).str[-8:].astype(int)
+        unique_num_id = df['cSenID'].unique()
+
+        for tmp_val in unique_num_id:
+            tmp_id_df = df.loc[df['cSenID'] == tmp_val]
+            save_path = os.path.join(args.SORT_PATH, str(tmp_val))
+            os.makedirs(save_path, exist_ok=True)
+
+            save_path = os.path.join(save_path, str(period))
+            os.makedirs(save_path, exist_ok=True)
+
+            save_file_prefix = str(tmp_val)
+            save_file = f"{save_file_prefix}_{tmp_id_df['cSenDate'].unique()[0]}_.csv"
+            save_file_path = os.path.join(save_path, save_file)
+
+            # if os.path.exists(save_file_path):
+            #     print(f'[Already Sorted] {save_file}')
+            # else:
+            tmp_id_df.to_csv(save_file_path, index=False)
+
+    print('Sort data by ID')
+
+    files_raw = glob.glob(os.path.join(raw_path, '*.csv'))
+    files_raw.sort()
+
+    ### if parallel
+    Parallel(n_jobs=-1)(
+        delayed(process_file)(file_path, args, period) for file_path in tqdm(files_raw)
+    )
+
+
+    '''
+    ### if single
+    file_name_list = [os.path.basename(f) for f in files_raw if os.path.isfile(f)]
+    for FILE_NAME in tqdm(file_name_list):
+        file_path = os.path.join(raw_path, FILE_NAME)
+
+        with open(file_path, 'rb') as f:
+            binary_data = f.read()
+        csv_string = binary_data.decode('utf-8').replace('\x00', '')
+
+        df = pd.read_csv(StringIO(csv_string), header=None, na_values=na_values, keep_default_na=True)
+        if df.shape[1] == 21:
+            df.columns = set_header('202201')
+        elif df.shape[1] == 22:
+            df.columns = set_header('202202')
+        # df.columns = args.HEADER_LIST
+        df['cSenID'] = df['cSenID'].apply(lambda x: int(str(x)[-8:]))
+        unique_num_id = df['cSenID'].unique()
+        for tmp_val in unique_num_id:
+            tmp_id_df = df.loc[df['cSenID'] == tmp_val]
+            save_path = args.SORT_PATH + str(tmp_val) + '/'
+            if not os.path.exists(args.SORT_PATH + str(tmp_val)):
+                os.mkdir(save_path)
+            save_path = os.path.join(save_path, str(period))
+            if not os.path.exists(save_path):
+                os.mkdir(save_path)
+
+            save_file_prefix = str(tmp_val)
+            save_file = save_file_prefix + '_' + tmp_id_df['cSenDate'].unique()[0] + "_.csv"
+            if os.path.exists(os.path.join(save_path, save_file)):
+                print('[Already Sorted] {}'.format(save_file))
+            else:
+                tmp_id_df.to_csv(os.path.join(save_path, save_file), index=False)
+    '''
+
+
+def create_dir_if_not_exists(path):
+    if not os.path.exists(path):
+        os.makedirs(path, exist_ok=True)
+
+
+def process_file(args, dir_name, path_name, target_period, tmp_NAME):
+    save_path = os.path.join(args.SAMPLE_PATH, dir_name, str(target_period))
+    create_dir_if_not_exists(save_path)
+    save_path = os.path.join(save_path, tmp_NAME)
+    tgt_path = os.path.join(path_name, tmp_NAME)
+    sample_data(args, tgt_path, save_path)
+
+
+if __name__ == "__main__":
+    args = set_parameters()
+
+    # target_period = args.COLLECT_PERIOD[2]
+    target_index = ['202406', '202407']
+    period_list = [i for i in target_index]
+    print('Data Preprocessing for {}'.format(period_list))
+
+    for target_period in period_list:
+        print('Period: {}'.format(target_period))
+
+        # set folder of rawdata files name as 'raw_202202'
+        DATA_PATH = 'data/' + 'raw_' + target_period + '/'
+
+        ####### Filtered by USER ID #######
+        sort_rawdata(args, DATA_PATH, target_period)
+        # eval_stat(args.SORT_PATH, target_period)
+        #
+        # sample data with velocity
+        # dir_list = os.listdir(args.SORT_PATH)
+        # dir_list.sort()
+        #
+        # ####### if parallel #######
+        # tasks = []
+        # for dir_name in tqdm(dir_list):
+        #     path_name = os.path.join(args.SORT_PATH, dir_name, target_period)
+        #     files = glob.glob(os.path.join(path_name, '*.csv'))
+        #     files.sort()
+        #     file_name_list = [os.path.basename(f) for f in files if os.path.isfile(f)]
+        #     for tmp_NAME in file_name_list:
+        #         tasks.append((args, dir_name, path_name, target_period, tmp_NAME))
+        #
+        # Parallel(n_jobs=-1)(delayed(process_file)(*task) for task in tasks)
+        ##########################
+        #
+        #
+        # # ####### if single #######
+        # # for dir_name in dir_list:
+        # #     path_name = os.path.join(args.SORT_PATH, dir_name, target_period)
+        # #     files = glob.glob(os.path.join(path_name, '*.csv'))
+        # #     files.sort()
+        # #     file_name_list = [os.path.basename(f) for f in files if os.path.isfile(f)]
+        # #     if len(files) != 0:
+        # #         files.sort()
+        # #         for tmp_NAME in file_name_list:
+        # #             save_path = os.path.join(args.SAMPLE_PATH, dir_name)
+        # #             if not os.path.exists(save_path):
+        # #                 os.mkdir(save_path)
+        # #             save_path = os.path.join(save_path, str(target_period))
+        # #             if not os.path.exists(save_path):
+        # #                 os.mkdir(save_path)
+        # #             save_path = os.path.join(save_path, tmp_NAME)
+        # #             tgt_path = os.path.join(path_name, tmp_NAME)
+        # #             sample_data(args, tgt_path, save_path)
+        # # ########################
+        #
+        # eval_stat(args.SAMPLE_PATH, target_period)
+    print('done')
